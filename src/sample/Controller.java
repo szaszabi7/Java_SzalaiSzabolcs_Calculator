dbdc5e871c28964dfa0f5aa34e7a45cb95a09186
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-12-02 11:42:24 +0100

    done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,81 +1,78 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
-import javafx.scene.control.TextField;
 
-import java.awt.*;
 import java.text.DecimalFormat;
 
 public class Controller {
     DecimalFormat df = new DecimalFormat("#.##");
     @FXML
-    private Spinner<Integer> textFieldSzam1;
+    private Spinner<Double> spinnerSzam1;
     @FXML
-    private Spinner<Integer> textFieldSzam2;
+    private Spinner<Double> spinnerSzam2;
     @FXML
     private Label labelEredmeny;
 
     public void btnPlusz() {
-        if (textFieldSzam1.getText().isEmpty() || textFieldSzam2.getText().isEmpty()) {
+        if (spinnerSzam1.getValue() == null || spinnerSzam2.getValue() == null) {
             labelEredmeny.setText("Minden mezőt ki kell tölteni");
         } else {
-            Double szam1 = Double.parseDouble(textFieldSzam1.getText());
-            Double szam2 = Double.parseDouble(textFieldSzam2.getText());
-            Double eredmeny = szam1 + szam2;
+            double szam1 = spinnerSzam1.getValue();
+            double szam2 = spinnerSzam2.getValue();
+            double eredmeny = szam1 + szam2;
 
             labelEredmeny.setText(Double.toString(eredmeny));
         }
     }
 
     public void btnMinusz() {
-        if (textFieldSzam1.getText().isEmpty() || textFieldSzam2.getText().isEmpty()) {
+        if (spinnerSzam1.getValue() == null || spinnerSzam2.getValue() == null) {
             labelEredmeny.setText("Minden mezőt ki kell tölteni");
         } else {
-            Double szam1 = Double.parseDouble(textFieldSzam1.getText());
-            Double szam2 = Double.parseDouble(textFieldSzam2.getText());
-            Double eredmeny = szam1 - szam2;
+            double szam1 = spinnerSzam1.getValue();
+            double szam2 = spinnerSzam2.getValue();
+            double eredmeny = szam1 - szam2;
 
             labelEredmeny.setText(Double.toString(eredmeny));
         }
     }
 
     public void btnSzor() {
-        if (textFieldSzam1.getText().isEmpty() || textFieldSzam2.getText().isEmpty()) {
+        if (spinnerSzam1.getValue() == null || spinnerSzam2.getValue() == null) {
             labelEredmeny.setText("Minden mezőt ki kell tölteni");
         } else {
-            Double szam1 = Double.parseDouble(textFieldSzam1.getText());
-            Double szam2 = Double.parseDouble(textFieldSzam2.getText());
-            Double eredmeny = szam1 * szam2;
+            double szam1 = spinnerSzam1.getValue();
+            double szam2 = spinnerSzam2.getValue();
+            double eredmeny = szam1 * szam2;
 
             labelEredmeny.setText(Double.toString(eredmeny));
         }
     }
 
-    public void btnMaradekosOszt() {
-        if (textFieldSzam1.getText().isEmpty() || textFieldSzam2.getText().isEmpty()) {
+    public void btnOszt() {
+        if (spinnerSzam1.getValue() == null || spinnerSzam2.getValue() == null) {
             labelEredmeny.setText("Minden mezőt ki kell tölteni");
         } else {
-            Double szam1 = Double.parseDouble(textFieldSzam1.getText());
-            Double szam2 = Double.parseDouble(textFieldSzam2.getText());
-            Double eredmeny = szam1 / szam2;
-
+            double szam1 = spinnerSzam1.getValue();
+            double szam2 = spinnerSzam2.getValue();
+            double eredmeny = szam1 / szam2;
 
             labelEredmeny.setText(df.format(eredmeny));
         }
     }
 
-    public void btnOszt() {
-        if (textFieldSzam1.getText().isEmpty() || textFieldSzam2.getText().isEmpty()) {
+    public void btnMaradekosOszt(ActionEvent actionEvent) {
+        if (spinnerSzam1.getValue() == null || spinnerSzam2.getValue() == null) {
             labelEredmeny.setText("Minden mezőt ki kell tölteni");
         } else {
-            Double szam1 = Double.parseDouble(textFieldSzam1.getText());
-            Double szam2 = Double.parseDouble(textFieldSzam2.getText());
-            Double eredmeny = szam1 % szam2;
+            double szam1 = spinnerSzam1.getValue();
+            double szam2 = spinnerSzam2.getValue();
+            double eredmeny = szam1 % szam2;
 
-            labelEredmeny.setText(Double.toString(eredmeny));
+            labelEredmeny.setText(df.format(eredmeny));
         }
     }
 }
